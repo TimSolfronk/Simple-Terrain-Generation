924b272bee3c75ef18088a804c8fe0197d0ce63f
Tim Solfronk <tim.solfronk@gmail.com>
2025-04-25 16:56:07 +0200

    Implemented first Chunk-Loading-Test

--- Assets/ChunkLoader.cs
+++ Assets/ChunkLoader.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class ChunkLoader : MonoBehaviour
 {
     public static ChunkLoader instance;
-    public float maxViewDst = 300;
+    private int maxViewDst = 3;
     public Transform player;
 
     public static Vector2 viewerPosition;
@@ -13,6 +13,7 @@ public class ChunkLoader : MonoBehaviour
     int chunksVisibleInViewDst;
 
     Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> lastFrameChunks = new List<TerrainChunk>();
 
     private void Awake()
     {
@@ -22,11 +23,23 @@ public class ChunkLoader : MonoBehaviour
     private void Start()
     {
         chunkSize = TerrainGenerationConfig.GetChunkSize();
-        chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
+        chunksVisibleInViewDst = maxViewDst;
+    }
+
+    private void Update()
+    {
+        viewerPosition = new Vector2(player.position.x, player.position.z);
+        UpdateVisibleChunks();
     }
 
     void UpdateVisibleChunks()
     {
+        for(int i = 0; i < lastFrameChunks.Count; i++)
+        {
+            lastFrameChunks[i].SetVisible(false);
+        }
+        lastFrameChunks.Clear();
+
         int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
         int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
 
@@ -36,29 +49,59 @@ public class ChunkLoader : MonoBehaviour
 
                 if(terrainChunkDictionary.ContainsKey(viewedChunkCoord))
                 {
-
+                    terrainChunkDictionary[viewedChunkCoord].UpdateChunk();
+                    if(terrainChunkDictionary[viewedChunkCoord].isVisible())
+                    {
+                        lastFrameChunks.Add(terrainChunkDictionary[viewedChunkCoord]);
+                    }
                 } else
                 {
                     terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord));
                 }
             }
         }
+
     }
 
+    public int GetMaxViewDistance()
+    {
+        return maxViewDst;
+    }
 
     public class TerrainChunk
     {
         GameObject meshObject;
         Vector2 position;
+        Bounds bounds;
+
         public TerrainChunk(Vector2 coord)
         {
             position = coord * TerrainGenerationConfig.GetChunkSize();
+            bounds = new Bounds(position, Vector2.one * TerrainGenerationConfig.GetChunkSize());
             Vector3 positionV3 = new Vector3(position.x, 0, position.y);
 
             meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
             meshObject.transform.position = positionV3;
             meshObject.transform.localScale = Vector3.one * TerrainGenerationConfig.GetChunkSize() / 10f;
-            meshObject.transform.parent = ChunkLoader.instance.transform;
+            meshObject.transform.parent = instance.transform;
+            SetVisible(false);
+        }
+
+        public void UpdateChunk()
+        {
+            float minViewerDst = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            bool visible = minViewerDst <= instance.GetMaxViewDistance() * TerrainGenerationConfig.GetChunkSize();
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool isVisible()
+        {
+            return meshObject.activeSelf;
         }
     }
 }
